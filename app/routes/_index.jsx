466d299be7dc74de18cdbde881bd92b712be1eda
WaficMikati <wrmikati@proton.me>
2025-11-14 22:01:14 -0500

    updating...

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,5 +1,10 @@
+export const meta = () => {
+  return [
+    { title: 'Home - React Router Template' },
+    { name: 'description', content: 'Welcome to My Template!' }
+  ]
+}
+
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  return <h1>Home</h1>
 }
--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -1,5 +1,10 @@
+export const meta = () => {
+  return [
+    { title: 'About - React Router Template' },
+    { name: 'description', content: 'About My Template' }
+  ]
+}
+
 export default function About() {
-  return (
-    <h1>About</h1>
-  )
+  return <h1>About</h1>
 }
